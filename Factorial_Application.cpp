8a55193978ce5a0b78fa533bcaf620600afc72a0
Roberto Chapa <bobby.chapa@gmail.com>
2019-02-12 06:52:01 -0500

    Update Factorial_Application.cpp
    Hello Seth,
    
    In this version, the function has arguments passed to it as well a return. A function should take arguments and have a return.

--- Factorial_Application.cpp
+++ Factorial_Application.cpp
@@ -1,49 +1,51 @@
-// Factorial_Application.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// C++ Program to find the 
+/ factorial_v2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/// factorial.cpp : Defines the entry point for the console application.
 
+//#include "stdafx.h"
 #include "pch.h"
-#include <iostream>
+#include "iostream"
 
 using namespace std;
 
-void factorial()
+long long findFactorial(long long f);
+
+int main()
 {
-	int x, i;
-	double f = 1;
+	bool t = true;
+	long long f;
 
-		cout << "Input Factorial: ";
-		cin >> x;
-		cout << endl;
-		f = x;
+	while (t == true)
+	{
+		cout << "Input f: " << endl;
+		cin >> f; //If user enters anything other than an integer f return 0
 
-		while (x > 1)
+		if (f == 0)
 		{
-			x--;
-			f = f * x;
-		}
+			t = false;
 
-		cout << "Factorial is equal to: " << f << endl;
-		cout << endl;
-	
+			cout << endl << "END" << endl << endl;
+		}
+		else
+		{
+			f = findFactorial(f);
+
+			cout << endl;
+			cout << "The factorial is " << f << endl << endl;
+		}
+	}
+
+	return 0;
 }
 
-	int main()
+long long findFactorial(long long f)
+{
+	long long p = 0, h = 1;
+
+	for (int i = 1; i < f + 1; i++)
 	{
-
-		char c;
-
-		while (true)
-		{
-			factorial();
-
-			cout << "Would you like to run the program again? y/n: ";
-			cin >> c;
-			cout << endl;
-
-			if (c == 'n' || c == 'N' || c == '0') break;
-
-			cout << endl << "====================================================" << endl << endl;
-			
-		}
-		return 0;
+		p = i * h; h = p;
 	}
+
+	return p;
+}
+
